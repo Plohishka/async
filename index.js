f58e08ca47957e9377d11f7fb8040b48c4c059ed
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-08-18 11:16:07 +0200

    async/await

---
+++ index.js
@@ -0,0 +1,10 @@
+
+async function getValue() {
+    try {
+        const promiseResult = await fetch('https://fakestoreapi.com/products');
+        const data = await promiseResult.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
